f0ddf64e82ec10d6a68df639c47fe0200d3209d6
Suizanna <73129581+Suizanna@users.noreply.github.com>
2021-07-26 07:00:26 +0300

    add

--- src/Components/Searchbar/Searchbar.jsx
+++ src/Components/Searchbar/Searchbar.jsx
@@ -1,11 +1,27 @@
 import s from "./SearchBar.module.css";
-
+// import { useState } from "react";
 const Searchbar = ({ onSetQuery, query, onSubmit }) => {
+    //  const [value, setValue] = useState("");
+
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+
+  //   //нет рендера картинок при пробеле если trim
+  //   if (value.trim() === '') { 
+  //   return alert("Enter search query");
+  //   }
+
+  //  onSubmit(value);
+  //   //очиста формы сразу после Submit 
+  //   setValue("");
+  // };
  
 
   return (
     <header className={s.Searchbar}>
-      <form onSubmit={onSubmit}  className={s.SearchForm}>
+      <form onSubmit={onSubmit}
+        // onSubmit={handleSubmit}
+        className={s.SearchForm}>
       <button type="submit" className={s.SearchForm_button}>
           <span className="SearchForm-button-label">Search</span>
         </button>
